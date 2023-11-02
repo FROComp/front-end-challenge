2bee3dc002130403033e44e3fcfe2789641be28e
FaBiUsK <fabiojunio385@gmail.com>
2023-11-02 00:20:13 -0300

    :wrench: add colors palette

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export default {
+  pink: "#E73980",
+  red: "#861040",
+  yellow: "#D18000",
+  background: "#fff",
+  textWhite: "#fff",
+  textDark: "#000",
+  textDeep: "#323232",
+  textGrey: "#646464",
+};
