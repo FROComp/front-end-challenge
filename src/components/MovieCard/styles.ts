a7a4f7ace092f148e867f136457550e5fba1afc1
FaBiUsK <fabiojunio385@gmail.com>
2023-11-02 13:23:14 -0300

    :sparkles: add new component MovieCard

---
+++ src/components/MovieCard/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import colors from "styles/colors";
+
+import { fontSizeXs, spacingInlineSm, fontWeightBold } from "styles/tokens";
+
+export const Box = styled.div`
+  background-color: ${colors.background};
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`;
+
+export const ImageContent = styled.div`
+  img {
+    width: 100%;
+  }
+`;
+
+export const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Text = styled.p`
+  font-size: ${fontSizeXs};
+  font-weight: ${fontWeightBold};
+  line-height: ${spacingInlineSm};
+  width: 100%;
+`;
+
+export const Title = styled(Text)`
+  color: ${colors.textDark};
+`;
+
+export const ReleaseDate = styled(Text)`
+  color: ${colors.textGrey};
+`;
