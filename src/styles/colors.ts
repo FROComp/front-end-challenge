963bb3277ab714f86378a066bdd254ed522baa71
FaBiUsK <fabiojunio385@gmail.com>
2023-11-02 14:46:51 -0300

    :lipstick: add new token colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,6 +2,8 @@ export default {
   pink: "#E73980",
   red: "#861040",
   yellow: "#D18000",
+  purple: "#5C16C5",
+  green: "#14FF00",
   background: "#fff",
   textWhite: "#fff",
   textDark: "#000",
