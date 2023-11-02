003ec96ddbfb3a0361e5de9456df78917ea2513d
FaBiUsK <fabiojunio385@gmail.com>
2023-11-01 23:59:02 -0300

    :arrow_up: add react-router-dom and initial routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>List page</h1>,
+  },
+  {
+    path: "/details/:id",
+    element: <h1>Details page</h1>,
+  },
+]);
+
 function Routes() {
-  return <h1>First page</h1>;
+  return <RouterProvider router={routes} />;
 }
 
 export default Routes;
