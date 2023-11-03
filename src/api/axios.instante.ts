fb683de9244c507fcd480a80f5d010cfe3d7a52d
FaBiUsK <fabiojunio385@gmail.com>
2023-11-02 22:35:15 -0300

    :wrench: add language in api instance

--- src/api/axios.instante.ts
+++ src/api/axios.instante.ts
@@ -1,13 +1,18 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const baseURL = import.meta.env.VITE_API_URL;
 const token = import.meta.env.VITE_API_TOKEN;
+const language = import.meta.env.VITE_API_LANGUAGE;
 
 const instance = axios.create({
   baseURL,
   headers: {
     Authorization: `Bearer ${token}`,
   },
+  params: {
+    language,
+  },
 });
 
+export type { AxiosResponse };
 export default instance;
