4ab450697631a12d04f4665c9ac0694ac9637b50
FaBiUsK <fabiojunio385@gmail.com>
2023-11-03 18:16:27 -0300

    :sparkles: update components to support service call

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,18 +1,39 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
 import * as S from "./styles";
 
 interface IMovieCardProps {
-  image: string;
+  id: number;
+  poster: string;
   title: string;
   release: string;
 }
 
-const MovieCard: React.FC<IMovieCardProps> = ({ image, title, release }) => {
+const MovieCard: React.FC<IMovieCardProps> = ({
+  id,
+  poster,
+  title,
+  release,
+}) => {
+  const navigate = useNavigate();
+
+  const handleGetMoviePosterUrl = (posterPath: string): string =>
+    `${import.meta.env.VITE_IMAGE_API_URL}/${posterPath}`;
+
+  const handleDetails = () => {
+    navigate({
+      pathname: `details/${id}`,
+    });
+  };
+
+  const image = handleGetMoviePosterUrl(poster);
+
   return (
-    <S.Box>
+    <S.Box onClick={handleDetails}>
       <S.ImageContent>
-        <img src={image} alt="Imagem do filme" />
+        <img src={image} alt="Imagem do filme" loading="lazy" />
       </S.ImageContent>
       <S.TextContent>
         <S.Title>{title}</S.Title>
