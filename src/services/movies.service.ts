c7f4280e0d7a524a673416471e5ce2ab4972fd2b
FaBiUsK <fabiojunio385@gmail.com>
2023-11-03 18:15:30 -0300

    :sparkles: add movie services

---
+++ src/services/movies.service.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosResponse } from "api/axios.instante";
+
+export const getPopularMoviesServices = (
+  page: number = 1
+): Promise<AxiosResponse<any>> =>
+  axios.get("/movie/popular", { params: { page } });
+
+export const getMoviesByGenresService = (
+  with_genres: string,
+  page: number = 1
+): Promise<AxiosResponse<any>> =>
+  axios.get("/discover/movie", { params: { with_genres, page } });
+
+export const getMovieDetailsService = (
+  movieId: number
+): Promise<AxiosResponse<any>> => axios.get(`/movie/${movieId}`);
