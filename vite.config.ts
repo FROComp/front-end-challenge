36624c861967d8ca46303bca9700df4241877e5a
FaBiUsK <fabiojunio385@gmail.com>
2023-11-03 14:18:18 -0300

    :wrench: add new alises

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,11 @@ export default defineConfig({
       components: `${path.resolve(__dirname, "./src/components/")}`,
       constants: `${path.resolve(__dirname, "./src/constants/")}`,
       contexts: `${path.resolve(__dirname, "./src/contexts/")}`,
+      hooks: `${path.resolve(__dirname, "./src/hooks/")}`,
+      pages: `${path.resolve(__dirname, "./src/pages/")}`,
       services: `${path.resolve(__dirname, "./src/services/")}`,
       styles: `${path.resolve(__dirname, "./src/styles/")}`,
-      pages: `${path.resolve(__dirname, "./src/pages/")}`,
+      utils: `${path.resolve(__dirname, "./src/utils/")}`,
     },
   },
 });
