3817dc656bfe07b102eb4353bed38c97ef292b99
FaBiUsK <fabiojunio385@gmail.com>
2023-11-04 12:50:11 -0300

    :sparkles: use context details data

--- src/components/DetailsBanner/index.tsx
+++ src/components/DetailsBanner/index.tsx
@@ -1,57 +1,35 @@
 import React from "react";
 
-import card from "assets/card-template.png";
-
 import AverageGraph from "components/AverageGraph";
+
+import useDetailsBanner from "./hook";
 
 import * as S from "./styles";
 
 const DetailsBanner: React.FC = () => {
+  const { poster, title, info, average, sinopse, crew } = useDetailsBanner();
+
   return (
     <S.Wrapper>
-      <S.Poster src={card} alt="Porter do filme" />
+      <S.Poster src={poster} alt="Porter do filme" loading="lazy" />
       <S.DetailsContent>
-        <S.DetailsTitle>Deadpool (2016)</S.DetailsTitle>
+        <S.DetailsTitle>{title}</S.DetailsTitle>
 
-        <S.DetailsInfo>
-          16 anos • 11/02/2016 (BR) • Ação, Aventura, Comédia, Ficção científica
-          • 1h 47m
-        </S.DetailsInfo>
+        <S.DetailsInfo>{info}</S.DetailsInfo>
 
-        <AverageGraph percentage={50} />
+        <AverageGraph percentage={average} />
 
         <S.DetailsSinopse>Sinopse</S.DetailsSinopse>
 
-        <S.DetailsDescription>
-          Baseado no anti-herói não convencional da Marvel Comics, Deadpool
-          conta a história da origem do ex-agente das Forças Especiais que se
-          tornou o mercenário Wade Wilson. Depois de ser submetido a um
-          desonesto experimento que o deixa com poderes de cura acelerada, Wade
-          adota o alter ego de Deadpool. Armado com suas novas habilidades e um
-          senso de humor negro e distorcido, Deadpool persegue o homem que quase
-          destruiu sua vida.
-        </S.DetailsDescription>
+        <S.DetailsDescription>{sinopse}</S.DetailsDescription>
 
         <S.DetailsTeamBox>
-          <S.DetailsTeam>
-            <S.DetailsTeamName>Rob Liefeld</S.DetailsTeamName>
-            <S.DetailsTeamRole>Characters</S.DetailsTeamRole>
-          </S.DetailsTeam>
-
-          <S.DetailsTeam>
-            <S.DetailsTeamName>Rob Liefeld</S.DetailsTeamName>
-            <S.DetailsTeamRole>Characters</S.DetailsTeamRole>
-          </S.DetailsTeam>
-
-          <S.DetailsTeam>
-            <S.DetailsTeamName>Rob Liefeld</S.DetailsTeamName>
-            <S.DetailsTeamRole>Characters</S.DetailsTeamRole>
-          </S.DetailsTeam>
-
-          <S.DetailsTeam>
-            <S.DetailsTeamName>Rob Liefeld</S.DetailsTeamName>
-            <S.DetailsTeamRole>Characters</S.DetailsTeamRole>
-          </S.DetailsTeam>
+          {crew?.map((crew) => (
+            <S.DetailsTeam>
+              <S.DetailsTeamName>{crew.original_name}</S.DetailsTeamName>
+              <S.DetailsTeamRole>{crew.job}</S.DetailsTeamRole>
+            </S.DetailsTeam>
+          ))}
         </S.DetailsTeamBox>
       </S.DetailsContent>
     </S.Wrapper>
