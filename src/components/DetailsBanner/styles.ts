a393aed43820413df28a521e4bed1f1d350584cf
FaBiUsK <fabiojunio385@gmail.com>
2023-11-03 22:58:56 -0300

    :sparkles: add details banner component

--- src/components/DetailsBanner/styles.ts
+++ src/components/DetailsBanner/styles.ts
@@ -4,16 +4,22 @@ import colors from "styles/colors";
 import {
   spacingInlineXxs,
   spacingInlineXs,
-  spacingInlineLg,
+  spacingInlineSm,
   spacingInlineMd,
+  spacingInlineLg,
   borderRadiusMd,
+  fontSizeXxs,
+  fontSizeXs,
+  fontSizeSm,
   fontSizeMd,
   fontSizeLg,
+  fontWeightRegular,
 } from "styles/tokens";
 
 export const Wrapper = styled.div`
   background-color: ${colors.red};
   display: flex;
+  justify-content: center;
   gap: ${spacingInlineMd};
   padding: 72px 112px;
   width: 100%;
@@ -43,7 +49,11 @@ export const DetailsContent = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
-  width: 100%;
+  max-width: 900px;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const DetailsTitle = styled.h1`
@@ -62,4 +72,53 @@ export const DetailsTitle = styled.h1`
 export const DetailsInfo = styled.p`
   color: ${colors.textWhite};
   font-size: 18px;
+  margin-bottom: ${spacingInlineXs};
+
+  @media (max-width: 768px) {
+    margin-bottom: ${spacingInlineMd};
+  }
+`;
+
+export const DetailsSinopse = styled.p`
+  color: ${colors.textWhite};
+  font-size: ${fontSizeSm};
+  margin-bottom: ${spacingInlineXxs};
+
+  @media (max-width: 768px) {
+    margin-bottom: ${spacingInlineMd};
+  }
+`;
+
+export const DetailsDescription = styled.p`
+  color: ${colors.textWhite};
+  font-size: ${fontSizeXs};
+  font-weight: ${fontWeightRegular};
+  margin-bottom: ${spacingInlineSm};
+
+  @media (max-width: 768px) {
+    margin-bottom: ${spacingInlineMd};
+  }
+`;
+
+export const DetailsTeamBox = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 100%;
+`;
+
+export const DetailsTeam = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const DetailsTeamName = styled(DetailsSinopse)`
+  font-size: ${fontSizeXs};
+
+  @media (max-width: 768px) {
+    margin-bottom: 0;
+  }
+`;
+
+export const DetailsTeamRole = styled(DetailsDescription)`
+  font-size: ${fontSizeXxs};
 `;
