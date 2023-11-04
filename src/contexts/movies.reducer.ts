0c4fa9ba360dd390747ae95c87aa7bb653220d0e
FaBiUsK <fabiojunio385@gmail.com>
2023-11-04 09:50:35 -0300

    :wrench: update movie context to use types

--- src/contexts/movies.reducer.ts
+++ src/contexts/movies.reducer.ts
@@ -1,37 +1,4 @@
-import { Dispatch, Reducer, ReducerAction } from "react";
-
-type DispatchType = Dispatch<ReducerAction<Reducer<any, any>>>;
-
-type Actions = {
-  type: string;
-  payload: any;
-};
-
-export type IGenre = {
-  id: number;
-  name: string;
-};
-
-export type IMovie = {
-  id: number;
-  original_title: string;
-  release_date: string;
-  poster_path: string;
-};
-
-export type MoviesContextType = {
-  contentIsLoading: boolean;
-  moviesIsLoading: boolean;
-  genres: IGenre[];
-  movies: IMovie[];
-  filters: {
-    genresIds: number[];
-    page: number;
-    maxPages: number;
-  };
-  handleSetGenresFilter: (item: number) => void;
-  handleSetPageFilter: (page: number) => void;
-};
+import { DispatchType, Actions, IGenre, IMovie } from "types/movie.types";
 
 export enum MoviesTypes {
   CONTENT_LOADING = "CONTENT_LOADING",
