34b0250c7162a07fb42ba33755f68a4e579ca45f
FaBiUsK <fabiojunio385@gmail.com>
2023-11-04 13:52:16 -0300

    :sparkles: add Movie list content skeleton

--- src/pages/Movies/components/MoviesBox/index.tsx
+++ src/pages/Movies/components/MoviesBox/index.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import { useMoviesContext } from "contexts/movies.context";
 
 import MovieList from "components/MovieList";
+import MovieBoxSkeleton from "../MovieBoxSkeleton";
 
 const MovieBox: React.FC = () => {
-  const { movies } = useMoviesContext();
+  const { movies, contentIsLoading } = useMoviesContext();
 
   return (
     <>
-      <MovieList movies={movies} />
+      {contentIsLoading ? <MovieBoxSkeleton /> : <MovieList movies={movies} />}
     </>
   );
 };
